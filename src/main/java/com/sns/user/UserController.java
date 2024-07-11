2237782c1115b1c38db6533f119a4fbf59092f19
DESKTOP-C5GBA64\user <test@test.com>
2024-07-11 18:49:55 +0900

    2024-07-11

---
+++ src/main/java/com/sns/user/UserController.java
@@ -0,0 +1,16 @@
+package com.sns.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/user")
+@Controller
+public class UserController {
+
+	@GetMapping("/sign-up-view")
+	public String signUpView() {
+		return "layout/defaultLayout";
+	}
+
+}
