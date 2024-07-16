ee94a0bedb4ea11c12539e16d4bcdaa57654d405
DESKTOP-C5GBA64\user <test@test.com>
2024-07-16 18:49:44 +0900

    2024-07-16

---
+++ src/main/java/com/sns/post/repository/PostRepository.java
@@ -0,0 +1,14 @@
+package com.sns.post.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sns.post.Entity.PostEntity;
+
+public interface PostRepository extends JpaRepository<PostEntity, Integer>{
+	
+	// JPQL
+	public List<PostEntity> findAllByOrderByIdDesc();
+
+}
--- src/main/java/com/sns/timeline/TimelineController.java
+++ src/main/java/com/sns/timeline/TimelineController.java
@@ -1,13 +1,34 @@
 package com.sns.timeline;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.sns.post.Entity.PostEntity;
+import com.sns.post.bo.PostBO;
+
+import jakarta.servlet.http.HttpSession;
 
 
 @Controller
 public class TimelineController {
+	
+	@Autowired
+	private PostBO postBO;
+	
 	@GetMapping("/timeline/timeline-view")
-	public String timelineView() {
+	public String timelineView(Model model) {
+		
+	
+		// DB Check - list<PostEntity>
+		List<PostEntity> postList = postBO.getPostEntityList();
+		
+		// model
+		model.addAttribute("postList", postList);
+		
 		return "timeline/timeline";
 	}
 }
